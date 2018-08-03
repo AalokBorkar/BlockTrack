4f8b5a6a2645cf83936603ea90bce2ef6dec8a30
AalokBorkar <34354063+AalokBorkar@users.noreply.github.com>
2018-08-02 22:49:37 -0700

    Update note.sol
    updated with function modifiers and more efficient data storage for ownership

--- note.sol
+++ note.sol
@@ -1,42 +1,31 @@
 pragma solidity ^0.4.4;
 
-contract Pass{
-    mapping(address => bool) ownership;
+contract Logistics{
+    
+    address public owner;
     mapping(address => string) notes;
     
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
     constructor(address genesis) public {
-        ownership[genesis] = true; //this address starts with it
+       owner = genesis;
     }
     
-    function checkOwnership(address p) public view returns(bool){
-        if(ownership[p]){
-            return true;
-        }
-        return false;
+    function sign(string signedNote) public onlyOwner{
+            notes[owner] = signedNote; //gaurenteed that msg.sender == owner
     }
     
-    function sign(string signedNote) public returns(uint){ // 1 on success 0 on fail
-        if(checkOwnership(msg.sender)){ //if msg.sender owns the note
-            notes[msg.sender] = signedNote;
-            return 1;
-        }
-        return 0;
+    function transferOwnership(address nuOwner) onlyOwner {
+        owner = nuOwner;
     }
-    
-    function pass(address recipient) public returns(uint){ // 1 on success 0 on fail
-        if(checkOwnership(msg.sender)){ //if msg.sender owns the note
-            ownership[msg.sender] = 0;
-            ownership[recipient] = 1;
-            return 1;
-        }
-        return 0;
-    }
-    
+
     function viewNotes(address participant) public returns(string){ // signed note on success nothing on fail
         if(notes[participant] !== 0){
             return (notes(participant));   
         }
     }
 
-    
 }
