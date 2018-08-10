13977b28558ab3e28a209876306865064bef67a7
AalokBorkar <34354063+AalokBorkar@users.noreply.github.com>
2018-08-09 21:55:39 -0700

    Fully functioning dApp :^)

--- dappGui.js
+++ dappGui.js
@@ -94,42 +94,72 @@ var contractABI = web3.eth.contract([
 	}
 ]);
 
-var Note = contractABI.at(0xea449D80E775607612Cc6d5ae9232EA10e417Ec1);
+var Note = contractABI.at('0xea449D80E775607612Cc6d5ae9232EA10e417Ec1');
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+var currentOwner;
+var doesOwn;
+setInterval(function(){
+	let hidecontent= new Promise(function(resolve, reject){ 
+		Note.owner.call(function(err, res){
+			if(!err){
+				currentOwner = res;
+				resolve('done');
+			} else{
+				console.log("Extracting current owner failed!");
+			}
+		});
+	});
+	hidecontent.then(function(){
+		if(web3.eth.defaultAccount !== currentOwner){ //if the current Metamask account is NOT the current holder of the ledger hide owner functionality
+			$('#sign').hide();
+			$('#transfer').hide();
+			doesOwn = false;
+		} else{
+			$('#sign').show();
+			$('#transfer').show();
+			doesOwn = true;
+		}
+	});
 
-// if(web3.eth.defaultAccount !== Note.owner){ //if the current Metamask account is NOT the current holder of the ledger hide owner functionality
-// 	$('#sign').hide();
-// 	$('#transfer').hide();
-// }
+}, 101);
 
+setInterval(function(){ //update current holder address every 100 milliseconds
+	let changeOwner = new Promise(function(resolve, reject){ 
+		Note.owner.call(function(err, res){
+			if(!err){
+				currentOwner = res;
+				resolve('done');
+			} else{
+				console.log("Extracting current owner failed!");
+			}
+		});
+	});
+	changeOwner.then(function(){
+		$('#ownerAddress').text(currentOwner);
+	});
+
+}, 100);
 
 $('#viewButton').click(function(){
+	
 	if ($('#viewInput').val()){ //if text input is populated
 		$('#view-input-error').hide();
 		$('#viewInput').removeClass("orange");
 		
-
-		Note.notes('0xf35f06208aCcaCF3FaF678df88A76142b923408e', function(err, res){
+		//alert(web3.eth.getBalance("0x353747f9D3EcA6F52f72e0C569e02aC18D8df442"));
+		Note.notes($('#viewInput').val(), function(err, res){
 			if(!err){
-				alert(res);
+				alert("Success!" + '\n' + "Transaction id: " + res);
 
 			} else{
 				console.log("Error fetching information from given address");
 			}
 
-
 		});
 
-		// Note.transferOwnership(0x353747f9D3EcA6F52f72e0C569e02aC18D8df442, function(err, res){
-		// 	if(!err){
-		// 		alert("Transfered!");
-		// 	} else{
-		// 		console.log("Error transfering to given address");
-		// 	}
 
-
-		// });
 	} else { //field is blank
 		$('#view-input-error').show();
 		$('#viewInput').addClass("orange");
@@ -138,7 +168,18 @@ $('#viewButton').click(function(){
 });
 
 $('#signButton').click(function(){
-	if ($('#signInput').val()){ //if text input is populated
+
+	if ($('#signInput').val() && doesOwn){ //if text input is populated and the owner is logged in
+
+		Note.sign($('#signInput').val(), function(err, res){
+			if(!err){
+				alert("Success!" + '\n' + "Transaction id: " + res);
+
+			} else{
+				console.log("Error signing the ledger!");
+			}
+
+		});
 
 		$('#sign-input-error').hide();
 		$('#signInput').removeClass("orange");
@@ -151,8 +192,18 @@ $('#signButton').click(function(){
 });
 
 $('#transferButton').click(function(){
-	if ($('#transferInput').val()){ //if text input is populated
-		
+	if ($('#transferInput').val() && doesOwn){ //if text input is populated and the owner is logged in
+
+		Note.transferOwnership($('#transferInput').val(), function(err, res){
+			if(!err){
+				alert("Success!" + '\n' + "Transaction id: " + res);
+
+			} else{
+				console.log("Error sending ledger to given address!");
+			}
+		});
+
+
 		$('#transfer-input-error').hide();
 		$('#transferInput').removeClass("orange");
 
@@ -162,7 +213,6 @@ $('#transferButton').click(function(){
 	}
 
 });
-
 
 
 
